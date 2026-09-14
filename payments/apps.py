ab906c7febfbdbc13c6c49f88393e7fbb4edbd6b
Quarj0 <54241472+quarj0@users.noreply.github.com>
2026-09-14 20:27:15 +0000

    feat(commerce): register marketplace payment models

--- payments/apps.py
+++ payments/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PaymentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "payments"
+
+    def import_models(self):
+        super().import_models()
+        from . import commerce_models  # noqa: F401
